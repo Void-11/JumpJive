d924432ee369f9976ab2b4a4d80e15a465bd3b1d
Void-11 <vivaanmishra777@gmail.com>
2024-10-20 22:45:26 +0530

    Project Created
    - Added Assets

---
+++ Source/JumpJive/JumpJive.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JumpJive.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JumpJive, "JumpJive" );
